a5be75a22ec98977a27529125432695a89c62feb
Siarhei Piashko <peshko.sergey@gmail.com>
2020-10-05 03:46:12 +0300

    refactoring

--- src/main/java/com/siarhei/jpaefficiencyexperiments/crudbase/entity/BaseJournalEntity.java
+++ src/main/java/com/siarhei/jpaefficiencyexperiments/crudbase/entity/BaseJournalEntity.java
@@ -2,19 +2,15 @@ package com.siarhei.jpaefficiencyexperiments.crudbase.entity;
 
 import lombok.Getter;
 import lombok.Setter;
-import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
-import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.persistence.Column;
 import javax.persistence.EntityListeners;
 import javax.persistence.MappedSuperclass;
 import javax.validation.constraints.NotNull;
 import java.time.LocalDateTime;
-import java.util.UUID;
 
 @Getter
 @Setter
@@ -25,23 +21,11 @@ public abstract class BaseJournalEntity extends BaseEntity {
     @NotNull
     @CreatedDate
     @Column(name = "created_at")
-    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
     private LocalDateTime createdAt;
-
-    @NotNull
-    @CreatedBy
-    @Column(name = "created_by")
-    private UUID createdBy;
 
     @NotNull
     @LastModifiedDate
     @Column(name = "updated_at")
-    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
     private LocalDateTime updatedAt;
-
-    @NotNull
-    @LastModifiedBy
-    @Column(name = "updated_by")
-    private UUID updatedBy;
 
 }
